881d8dd49239effc4bda52ac27d43fa739f91b40
Benjamin Barbeau <benjamin@barbeau.net>
2023-06-29 07:36:13 -0400

    Update get unpacked dir

--- Ategon-QuickRestartMod/mod_main.gd
+++ Ategon-QuickRestartMod/mod_main.gd
@@ -3,7 +3,7 @@ extends Node
 const MOD_DIR := "Ategon-QuickRestartMod/"
 
 func _init(modLoader: ModLoader):
-	var dir = modLoader.UNPACKED_DIR + MOD_DIR
-	var ext_dir = dir + "extensions/"
+	var dir = ModLoaderMod.get_unpacked_dir() + MOD_DIR
+	var ext_dir = dir + "extensions"
 	
 	ModLoaderMod.install_script_extension("%s/LevelStage.gd" % [ext_dir])
